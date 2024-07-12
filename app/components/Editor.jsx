c45b17fc126c313f8a936053bba4f8abbf731851
jebarsonponraj <jebarsonponraj@gmail.com>
2024-07-12 21:57:54 +0530

    Fixed alignment of texts

--- app/components/Editor.jsx
+++ app/components/Editor.jsx
@@ -20,17 +20,17 @@ const initialInputTextArr = [
   {
     input1: {
       text: `"I can do all things through Christ who strengthens me."`,
-      fontSize: 20,
-      xAxis: 91,
-      yAxis: 235,
+      fontSize: 17,
+      xAxis: 77,
+      yAxis: 198,
       color: "#403a31",
       rotation: 0,
     },
     input2: {
       text: "- Philippians 4:13",
       fontSize: 14,
-      xAxis: 231,
-      yAxis: 267,
+      xAxis: 186,
+      yAxis: 225,
       color: "#403a31",
       rotation: 0,
 
@@ -39,9 +39,9 @@ const initialInputTextArr = [
   {
     input1: {
       text: `"When you can't see God's hand, trust His heart. He's working all things together for your good, shaping your story with purpose and love. Stay faithful, for His blessings are on the way."`,
-      fontSize: 14,
-      xAxis: 72,
-      yAxis: 313,
+      fontSize: 13,
+      xAxis: 73,
+      yAxis: 266,
       color: "#282020",
       rotation: 342,
 
@@ -59,9 +59,9 @@ const initialInputTextArr = [
   {
     input1: {
       text: "D A Y  5 0",
-      fontSize: 14,
-      xAxis: 167,
-      yAxis: 120,
+      fontSize: 13,
+      xAxis: 132,
+      yAxis: 110,
       color: "#403832",
       rotation: 0,
 
@@ -80,29 +80,29 @@ waste another hour of your precious time
 on what's not in your best interest.
 Life is not turning its back on you, so you
 must learn to not turn your back on it.`,
-      fontSize: 12,
-      xAxis: 87,
-      yAxis: 128,
-      color: "#403832",
-      rotation: 0,
+      fontSize: 10,
+      xAxis: 77,
+      yAxis: 127,
+      color: "#050404",
+      rotation: 359,
 
     },
   },
   {
     input1: {
       text: "I will never leave you",
-      fontSize: 18,
-      xAxis: 101,
-      yAxis: 144,
+      fontSize: 16,
+      xAxis: 80,
+      yAxis: 132,
       color: "#20274b",
       rotation: 0,
 
     },
     input2: {
       text: "nor forsake you.",
-      fontSize: 18,
-      xAxis: 118,
-      yAxis: 144,
+      fontSize: 16,
+      xAxis: 99,
+      yAxis: 135,
       color: "#20274b",
       rotation: 0,
 
@@ -111,8 +111,8 @@ must learn to not turn your back on it.`,
   {
     input1: {
       text: "Believe in yourself, you are unstoppable.",
-      fontSize: 16,
-      xAxis: 73,
+      fontSize: 14,
+      xAxis: 60,
       yAxis: 256,
       color: "#25211e",
       rotation: 7,
@@ -120,9 +120,9 @@ must learn to not turn your back on it.`,
     },
     input2: {
       text: "Keep pushing forward, never give up.",
-      fontSize: 16,
-      xAxis: 71,
-      yAxis: 260,
+      fontSize: 14,
+      xAxis: 56,
+      yAxis: 256,
       color: "#25211e",
       rotation: 7,
 
@@ -132,8 +132,8 @@ must learn to not turn your back on it.`,
     input1: {
       text: "The Seed of Faith",
       fontSize: 14,
-      xAxis: 199,
-      yAxis: 193,
+      xAxis: 165,
+      yAxis: 192,
       color: "#000000",
       rotation: 18,
 
@@ -146,11 +146,11 @@ The grandmother smiled gently and said, "These seeds may seem small now, but wit
 Years passed, and the child grew into an adult, facing challenges and uncertainties. Remembering her grandmother's wisdom, she planted seeds of hope and trust in her heart, nurturing them with faith.
 
 Just as her grandmother's garden bloomed with vibrant flowers, her life blossomed with resilience and joy, proving that from a tiny seed of faith, miracles can grow.`,
-      fontSize: 13,
-      xAxis: 113,
-      yAxis: 193,
+      fontSize: 12,
+      xAxis: 104,
+      yAxis: 155,
       color: "#000000",
-      rotation: 19,
+      rotation: 18,
 
     },
   },
@@ -158,9 +158,9 @@ Just as her grandmother's garden bloomed with vibrant flowers, her life blossome
     input1: {
       text: `"The future belongs to those who believe 
 in the beauty of their dreams."`,
-      fontSize: 16,
-      xAxis: 64,
-      yAxis: 243,
+      fontSize: 14,
+      xAxis: 52,
+      yAxis: 223,
       color: "#4e5360",
       rotation: 0,
 
@@ -270,10 +270,9 @@ const Editor = () => {
           </div>
           <div className="relative font-serif" style={{ overflow: "hidden" }} ref={previewRef}>
             <Image
-            className="object-fill"
               src={selectedImage}
-              width={350}
-              height={350}
+              width={300}
+              height={300}
               alt="selected book"
             />
              {inputData.input1 && (
@@ -304,7 +303,7 @@ const Editor = () => {
                   transform: `rotate(${inputData.input2.rotation}deg)`,
                   transformOrigin: "top left",
                   whiteSpace: "pre-wrap",
-                  lineHeight: "1",
+                  lineHeight: "1.2",
                   mixBlendMode: "multiply",
                 }}
                 dangerouslySetInnerHTML={{ __html: inputData.input2.text.replace(/\n/g, '<br />') }}
